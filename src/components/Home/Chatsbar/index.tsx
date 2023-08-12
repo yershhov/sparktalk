a5cd9a7af600913acf3e9b06ae3fdf0126e65819
yershhov <yershhov@gmail.com>
2023-08-12 16:28:41 +0200

    optimized resizer to not react before mouse down in it's area

--- src/components/Home/Chatsbar/index.tsx
+++ src/components/Home/Chatsbar/index.tsx
@@ -6,38 +6,54 @@ import { SidebarStyled, SidebarContent, SidebarResizer } from "./styled";
 
 export default function Sidebar() {
   const sidebarRef = useRef(null);
-  const [isResizing, setIsResizing] = useState(false);
+  const resizerRef = useRef(null);
+
   const [sidebarWidth, setSidebarWidth] = useState(300);
+  const [mouseDownWithinResizer, setMouseDownWithinResizer] = useState(false);
 
   const matches = useMediaQuery(screens.md);
 
-  const startResizing = useCallback(() => {
-    setIsResizing(true);
-  }, []);
-
   const stopResizing = useCallback(() => {
-    setIsResizing(false);
+    setMouseDownWithinResizer(false);
   }, []);
 
   const resize = useCallback(
     (mouseMoveEvent: MouseEvent) => {
-      console.log("resizing");
-
-      if (isResizing) {
+      if (mouseDownWithinResizer) {
         setSidebarWidth(
           mouseMoveEvent.clientX -
             (sidebarRef.current! as HTMLElement).getBoundingClientRect().left
         );
       }
     },
-    [isResizing]
+    [mouseDownWithinResizer]
   );
 
+  const mouseDown = useCallback((mouseDown: MouseEvent) => {
+    const { left, top, width, height } = (
+      resizerRef.current! as HTMLDivElement
+    ).getBoundingClientRect();
+
+    const { clientX, clientY } = mouseDown;
+
+    const isInside =
+      clientX >= left &&
+      clientX <= left + width &&
+      clientY >= top &&
+      clientY <= top + height;
+
+    if (isInside) {
+      setMouseDownWithinResizer(true);
+    }
+  }, []);
+
   useEffect(() => {
+    window.addEventListener("mousedown", mouseDown);
     window.addEventListener("mousemove", resize);
     window.addEventListener("mouseup", stopResizing);
 
     return () => {
+      window.addEventListener("mousedown", mouseDown);
       window.removeEventListener("mousemove", resize);
       window.removeEventListener("mouseup", stopResizing);
     };
@@ -53,7 +69,7 @@ export default function Sidebar() {
         <LogOutButton width="100%" />
         <div style={{ height: "5rem", width: "100%", background: "gray" }} />
       </SidebarContent>
-      {matches && <SidebarResizer onMouseDown={startResizing} />}
+      {matches && <SidebarResizer ref={resizerRef} />}
     </SidebarStyled>
   );
 }
